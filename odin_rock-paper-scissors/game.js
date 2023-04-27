1dd72f95e9a4b3a123f1559048f8418c191da6c2
reece-vlr <reece.vlr@gmail.com>
2023-04-28 00:16:12 +0800

    Add boilerplate

---
+++ odin_rock-paper-scissors/game.js
@@ -0,0 +1,20 @@
+console.log("Hello, World!");
+
+// to update, forgot how to jS fuCK
+
+function playRound(playerSelection, computerSelection) {
+    /*
+        plays a single round
+        returns String that declares the winner
+        playerSelection (player's choice) must be case insensitive
+    */
+}
+
+function getComputerChoice() {
+    // return either 'Rock', 'Paper', or 'Scissors'
+}
+
+// rock, paper, or scissors
+const playerSelection = "";
+const computerSelection = getComputerChoice();
+console.log(playRound(playRound(playerSelection, computerSelection)));
