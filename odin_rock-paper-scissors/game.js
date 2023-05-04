f77718f35a0b1ec1d0b358952f57c3935ee11022
reece-vlr <reece.vlr@gmail.com>
2023-05-04 22:54:46 +0800

    Add game function
    - Add game() which loops, playRound() five times
    - Update playRound()
        - simpler code
        - increments respective winner's count per round
    - Tidied up code, and bug fixes

--- odin_rock-paper-scissors/game.js
+++ odin_rock-paper-scissors/game.js
@@ -1,58 +1,83 @@
-let playerSelection;
-let computerSelection;
-
 let computerWins = 0;
 let playerWins = 0;
 
 // Functions
-function getComputerChoice() {
-    let choice = Math.random();
+function getComputerSelection() {
+    let computerSelection = Math.floor(Math.random() * 3);
+    // console.log(computerSelection);
 
-    if (choice < 0.34) 
-        return 'Rock';
-    else if (choice >= 0.34 && choice < 0.67) 
-        return 'Paper';
-    else
-        return 'Scissors';
+    switch (computerSelection) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerSelection.toLowerCase();
     playerSelection = playerSelection.toLowerCase();
 
+    let roundWinner = 'draw';
+
     if (playerSelection === computerSelection) {
-        return 'Draw!';
+        return roundWinner;
     }
-    
-    // Player vs. Computer results
-    if (playerSelection === 'rock') {
-        if (computerSelection === 'paper') {
-            computerWins++;
-            return 'Computer wins!';
-        }
-        else {
-            playerWins++;
-            return 'You win!';
-        }
+
+    // Player win conditions
+    if (playerSelection === 'rock' && computerSelection === 'scissors' || 
+        playerSelection === 'paper' && computerSelection === 'rock' || 
+        playerSelection === 'scissors' && computerSelection === 'paper'
+    ) {
+        playerWins++;
+        return roundWinner = 'player';
     }
-    else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') {
-            playerWins++;
-            return 'You win!';
-        }
-        else {
-            computerWins++;
-            return 'Computer wins!';
-        }
-    }
-    else if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock') {
-            computerWins++;
-            return 'Computer wins!';
-        }
-        else {
-            playerWins++;
-            return 'You win!';
-        }
+
+    // Computer win conditions
+    if (computerSelection === 'rock' && playerSelection === 'scissors' || 
+        computerSelection === 'paper' && playerSelection === 'rock' || 
+        computerSelection === 'scissors' && playerSelection === 'paper'
+    ) {
+        computerWins++;
+        return roundWinner = 'computer';
     }
 }
+
+function game() {
+    let playerSelection;
+    let computerSelection;
+
+    // Loops for 5 games
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt('What is thy selection?', '');
+        computerSelection = getComputerSelection();
+
+        switch (playRound(playerSelection, computerSelection)) {
+            case 'player':
+                console.log('The player wins!');
+                break;
+            case 'computer':
+                console.log('A computer win!');
+                break;
+            default:
+                console.log('Draw!');
+        }
+    }
+
+    // Display winner thru console.log
+    if (playerWins < computerWins) {
+        console.log(`Computer wins! With a total of ${computerWins} points!`);
+    }
+    else {
+        console.log(`Player wins! With a total of ${playerWins} points!`);
+    }
+}
+
+game();
+
+/* Validation checks
+console.log(playerWins);
+console.log(computerWins);
+*/
