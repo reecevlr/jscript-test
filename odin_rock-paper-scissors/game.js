d8779e0cd719406cfaac0765e7169c7b33493297
reece-vlr <reece.vlr@gmail.com>
2023-05-04 22:12:26 +0800

    Update playRound function
    Increments respective winner per round

--- odin_rock-paper-scissors/game.js
+++ odin_rock-paper-scissors/game.js
@@ -1,5 +1,8 @@
 let playerSelection;
-let computerSelection = getComputerChoice();
+let computerSelection;
+
+let computerWins = 0;
+let playerWins = 0;
 
 // Functions
 function getComputerChoice() {
@@ -23,21 +26,33 @@ function playRound(playerSelection, computerSelection) {
     
     // Player vs. Computer results
     if (playerSelection === 'rock') {
-        if (computerSelection === 'paper')
+        if (computerSelection === 'paper') {
+            computerWins++;
             return 'Computer wins!';
-        else
+        }
+        else {
+            playerWins++;
             return 'You win!';
+        }
     }
     else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') 
+        if (computerSelection === 'rock') {
+            playerWins++;
             return 'You win!';
-        else
+        }
+        else {
+            computerWins++;
             return 'Computer wins!';
+        }
     }
     else if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock')
-            return 'Computer wins!'
-        else 
+        if (computerSelection === 'rock') {
+            computerWins++;
+            return 'Computer wins!';
+        }
+        else {
+            playerWins++;
             return 'You win!';
+        }
     }
 }
